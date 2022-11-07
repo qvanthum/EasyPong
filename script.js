a7697384a3ab132b5591e3b628b443b8b7f6765d
Evert <evevrtsaarnak@gmail.com>
2022-11-08 00:33:29 +0200

    Worked on HTML structure, configured some style and added some basic things to script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+var canvas = document.getElementById("gamecanvas");
+var ctx = canvas.getContext("2d");
+const x = canvas.width;
+const y = canvas.height;
+const paddleHeight = 75;
+const paddleWidth = 10;
+const paddleStartingPosition = y/2-paddleHeight/2;
+var player1 = paddleStartingPosition;
+
+ctx.font = "32px Impact, fantasy";
+ctx.textBaseline = "center";
+ctx.textAlign = 'center';
+ctx.fillStyle = "#303F42";
+ctx.fillText("CLICK ENTER TO START", x/2, y/2);
+
+
+
+document.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        ctx.clearRect(0, 0, 800, 400);
+        ctx.fillRect(10, paddleStartingPosition, paddleWidth, paddleHeight)
+        ctx.fillRect(x-20, paddleStartingPosition , paddleWidth, paddleHeight)
+    }
+});
+
+document.addEventListener('keydown', function(e){
+    if(e.key === 'w')
+    ctx.clearRect(0, 0, 800, 400)
+    player1 = player1 + 1
+    ctx.fillRect(10, player1, paddleWidth, paddleHeight)
+ })
+
+
+
+
 
